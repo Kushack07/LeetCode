46f924772ad60dc7ea8a4618a23b94378df9c382
KUSH <kush18092004@gmail.com>
2025-11-13 19:13:19 +0530

    Time: 235 ms (2.10%) | Memory: 30.9 MB (5.48%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -4,17 +4,16 @@ public:
         int l=nums.size();
         int t=l-k%l;
         vector<int>ans;
-        for(int i=t;i<nums.size();i++)
+        for(int i=t;i<l;i++)
         {
             ans.push_back(nums[i]);
-            //cout<<nums[i]<<" ";
+            cout<<nums[i]<<" ";
         }
         for(int i=0;i<t;i++)
         {
-            //cout<<nums[i]<<" ";
-            ans.push_back(nums[i]); 
-        }
+            cout<<nums[i]<<" "; 
+            ans.push_back(nums[i]);
+        }
         nums=ans;
-
-    }
+    }
 };
