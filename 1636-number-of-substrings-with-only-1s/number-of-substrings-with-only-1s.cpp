1b2d0b8335c23d0ad8cbd4f0350ff70059a7519f
KUSH <kush18092004@gmail.com>
2025-11-17 00:47:00 +0530

    Time: 3 ms (33.16%) | Memory: 11.7 MB (11.57%) - LeetSync

--- 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.cpp
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int numSub(string s) {
-        const int mod =1e9+7;
-        typedef long long ll;
-        int left =0,count = 0; 
-        for(int right= 0 ; right<s.length();right++){
-            if(s[right]=='0')left = right +1;
-            else count = (count+(right-left+1)%mod)%mod;
+        int cnt = 0, total = 0, mod = 1e9 + 7;
+        for (char a : s) {
+            if (a == '1') {
+                cnt++;
+            } else {
+                cnt = 0;
+            }
+            total = (total + cnt) % mod;
         }
-        return count;
+        return total;
     }
 };
