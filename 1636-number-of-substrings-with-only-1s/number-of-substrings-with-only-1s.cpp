0313f4c94940117b20a6928aa34cac267160bc86
KUSH <kush18092004@gmail.com>
2025-11-16 17:36:58 +0530

    Time: 4 ms (19.79%) | Memory: 11.5 MB (95.37%) - LeetSync

---
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numSub(string s) {
+        const int mod =1e9+7;
+        int left =0,count = 0; 
+        for(int right= 0 ; right<s.length();right++){
+            if(s[right]=='0')left = right +1;
+            else count = (count+(right-left+1)%mod)%mod;
+        }
+        return count;
+    }
+};
