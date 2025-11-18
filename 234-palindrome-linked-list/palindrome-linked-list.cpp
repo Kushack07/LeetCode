671bdf31d4eb6c7615b7458035f884ce528f3bdc
KUSH <kush18092004@gmail.com>
2025-11-18 21:42:07 +0530

    Time: 0 ms (100.00%) | Memory: 132 MB (19.07%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -1,41 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        if (!head || !head->next)
-            return true;
-
-        // Step 1: Find middle
-        ListNode* slow = head;
-        ListNode* fast = head;
-
-        while (fast != nullptr && fast->next != nullptr) {
-            slow = slow->next;
-            fast = fast->next->next;
+        vector<int>ans;
+        while(head != nullptr){
+            ans.push_back(head->val);
+            head= head->next;
         }
-
-        // Step 2: Reverse second half
-        ListNode* prev = nullptr;
-        ListNode* curr = slow;
-        while (curr != nullptr) {
-            ListNode* nextTemp = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = nextTemp;
+        int left = 0 ;
+        int right = ans.size()-1;
+        while(left <right){
+        if(ans[left]!=ans[right]){
+            return false;
         }
-
-        // prev now points to the head of reversed second half
-
-        // Step 3: Compare first half and second half
-        ListNode* left = head;
-        ListNode* right = prev;
-
-        while (right != nullptr) {   
-            if (left->val != right->val)
-                return false;
-            left = left->next;
-            right = right->next;
+            left ++ ;
+            right -- ;
         }
-
         return true;
     }
 };
