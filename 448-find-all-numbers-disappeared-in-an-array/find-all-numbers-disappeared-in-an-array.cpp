6230ea883272e93da1e548dec6d0bd03920bef36
KUSH <kush18092004@gmail.com>
2025-11-23 02:08:43 +0530

    Time: 7 ms (47.94%) | Memory: 53.1 MB (57.77%) - LeetSync

---
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    void cyclicSort(vector<int>& nums)
+    {
+        int i=0;
+        while(i<nums.size())
+        {
+            if(nums[i] != nums[nums[i]-1])
+                swap(nums[i],nums[nums[i]-1]);
+            else
+                i++;
+        }
+    }
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) 
+    {
+        vector<int> ans;
+        cyclicSort(nums);
+        for(int i=0; i<nums.size(); i++)
+            if(nums[i] != i+1)
+                ans.push_back(i+1);
+        return ans;
+    }
+};
+
