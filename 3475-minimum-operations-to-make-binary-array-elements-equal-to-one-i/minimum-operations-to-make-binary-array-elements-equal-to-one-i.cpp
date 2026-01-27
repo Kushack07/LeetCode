2f3e6c52c130458bf01b14f4126c7623d2e1e91e
KUSH <kush18092004@gmail.com>
2026-01-27 22:51:34 +0530

    Time: 5 ms (70.78%) | Memory: 140 MB (75.97%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        int n = nums.size();
-        int cnt=0; 
-    
+        int n= nums.size();
+        int cnt=0 ; 
         for(int i=0;i<n-2;i++){
-    
-        if(nums[i]==0)
-        {
-            nums[i+1]^=1;
-            nums[i+2]^=1;
-            //cnt+;
-            cnt++;
-        }
-        }
-         if(nums[n-1]==0 || nums[n-2]==0){
-                return -1;
+            if(nums[i]==0){
+                nums[i+1]^=1;
+                nums[i+2]^=1;
+                cnt ++ ;
             }
-        
-        return cnt;
+        }
+        if(nums[n-1]==0||nums[n-2]==0){
+            return -1;
+        }
+        return cnt; 
     }
 };
