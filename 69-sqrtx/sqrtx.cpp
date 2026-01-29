ab6cf26db8ce143b345e4e40e36ed62812c37a0f
KUSH <kush18092004@gmail.com>
2026-01-29 09:55:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (13.28%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,6 +1,48 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return pow(x,0.5);
+        if (x < 2) return x;
+
+        int left = 1, right = x / 2;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long long square = (long long) mid * mid;
+
+            if (square == x) {
+                return mid;
+            } else if (square < x) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return right;        
     }
 };
+/*
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x < 2) return x;
+
+        int left = 1, right = x / 2;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long long square = (long long) mid * mid;
+
+            if (square == x) {
+                return mid;
+            } else if (square < x) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return right;        
+    }
+};
+*/
