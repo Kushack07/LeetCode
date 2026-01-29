0832ca5c198b5f7ea4e4a02c444da936561914be
KUSH <kush18092004@gmail.com>
2026-01-29 10:38:33 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (97.74%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-     // prevents runtime error
-
         int n = matrix.size();      
         int m = matrix[0].size();   
 
