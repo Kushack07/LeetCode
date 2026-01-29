20b0505a7efc39875742b863faf142c960521694
KUSH <kush18092004@gmail.com>
2026-01-29 14:21:36 +0530

    Time: 9 ms (45.72%) | Memory: 23 MB (11.83%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -5,12 +5,12 @@ public:
         int high = *max_element(piles.begin(), piles.end());
         
         while(low < high) {
-            int mid = (low + high) / 2; // trial speed k
+            int mid = (low + high) / 2; 
             int hours = 0;
 
             // Calculate total hours for this k
-            for(int p : piles) {
-                hours += (p + mid - 1) / mid; // ceil(p/mid)
+            for(int pk: piles) {
+                hours += (pk+ mid - 1) / mid; // ceil(p/mid)
             }
 
             if(hours <= h)
