653faab3e3d92de39164e755018d3aace94e7837
KUSH <kush18092004@gmail.com>
2026-01-31 03:28:14 +0530

    Time: 3 ms (38.02%) | Memory: 42.3 MB (16.26%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,21 +1,25 @@
+// C++ Solution
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> ans(n);
-        vector<int> left_Product(n);
-        vector<int> right_Product(n);
-        left_Product[0] = 1;
-        for(int i=1; i<n; i++){
-            left_Product[i] = left_Product[i-1] * nums[i-1];
+        vector<int> leftProducts(n, 1), rightProducts(n, 1), result(n, 0);
+        
+        // Calculate products of all elements to the left
+        for (int i = 1; i < n; i++) {
+            leftProducts[i] = leftProducts[i - 1] * nums[i - 1];
         }
-        right_Product[n-1] = 1;
-        for(int i=n-2; i>=0; i--){
-            right_Product[i] = right_Product[i+1] * nums[i+1];
+        
+        // Calculate products of all elements to the right
+        for (int i = n - 2; i >= 0; i--) {
+            rightProducts[i] = rightProducts[i + 1] * nums[i + 1];
         }
-        for(int i=0; i<n; i++){
-            ans[i] = left_Product[i] * right_Product[i];
+        
+        // Multiply left and right products
+        for (int i = 0; i < n; i++) {
+            result[i] = leftProducts[i] * rightProducts[i];
         }
-        return ans;
+        
+        return result;
     }
 };
