aac031de8ce0c3ffae335756271c2e349d2fb007
KUSH <kush18092004@gmail.com>
2026-02-02 01:03:52 +0530

    Time: 4 ms (63.58%) | Memory: 14.3 MB (31.53%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string convert(string s, int nr) {
+        if(nr<=1)return s; 
+        vector<string>v(nr,"");
+        int j=0,dir =-1; 
+        for(int i =0;i<s.length();i++){
+            if(j==nr-1 || j==0) dir *= (-1);
+            v[j] += s[i];
+            if(dir == 1)j++;
+            else j--;
+        }
+        string res; 
+        for(auto &it:v)res+=it;
+        return res; 
+    }
+};
