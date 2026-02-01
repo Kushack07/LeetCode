82c648e075f19c98a1561fa6f94eeca2d9330815
KUSH <kush18092004@gmail.com>
2026-02-01 17:34:43 +0530

    Time: 3 ms (6.53%) | Memory: 30.6 MB (17.09%) - LeetSync

--- 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.cpp
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.cpp
@@ -3,7 +3,7 @@ public:
     int minimumCost(vector<int>& nums) {
         int n= nums.size();
         sort(nums.begin()+1,nums.end());
-        int min = nums[0];
-        return min + nums[1]+nums[2];
+        int ans = nums[0];
+        return ans+nums[1]+nums[2];
     }
 };
