8e3b860400721c8b4f2c35d62a1ecfb38f4a333d
KUSH <kush18092004@gmail.com>
2026-02-08 01:10:11 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (87.42%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-
-        for(char c : s) {
-            if(c == '(' || c == '{' || c == '[') {
-                st.push(c);
-            }
-            else {
-                if(st.empty()) return false;
-
+        stack<char>st;
+        for(char ch : s){
+            if(ch =='(' || ch =='{'|| ch =='['){
+                st.push(ch);
+            }else{
+                if(st.empty())return false;
                 char top = st.top();
-
-                if( (c == ')' && top != '(') ||
-                    (c == '}' && top != '{') ||
-                    (c == ']' && top != '[') )
-                    return false;
-
                 st.pop();
+                if((ch == ')' && top !='(')||
+                    (ch =='}' && top != '{')||
+                    (ch == ']' && top !='[')){
+                        return false;
+                    }
             }
         }
-
         return st.empty();
     }
 };
