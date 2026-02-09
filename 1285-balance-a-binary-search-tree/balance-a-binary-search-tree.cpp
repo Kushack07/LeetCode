b3adb14f0c3d5b2f2878152df1c4a21d290cce4c
KUSH <kush18092004@gmail.com>
2026-02-09 20:59:04 +0530

    Time: 17 ms (39.85%) | Memory: 65.7 MB (42.05%) - LeetSync

--- 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
@@ -1,36 +1,25 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
+private:
+    vector<int> nums;
 public:
-void in(TreeNode* r,vector<int>&a)
-{
-    if(!r) return;
-    in(r->left,a);
-    a.push_back(r->val);
-    in(r->right,a);
-}
-TreeNode* bs(vector<int>&a,int l,int r)
-{
-    if(l>r) return nullptr;
-    int m=(l+r)/2;
-    TreeNode* nn=new TreeNode(a[m]);
-    nn->left=bs(a,l,m-1);
-    nn->right=bs(a,m+1,r);
-    return nn;
-}
-    TreeNode* balanceBST(TreeNode* r) {
-        if(!r) return nullptr;
-        vector<int>a;
-        in(r,a);
-        return bs(a,0,a.size()-1);
+    TreeNode* balanceBST(TreeNode* root) {
+        nums.clear();
+        getNumbers(root);
+        return balanceTree(0, nums.size() - 1);
+    }
+private:
+    void getNumbers(TreeNode* node) {
+        if (node == nullptr) return;
+        getNumbers(node->left);
+        nums.push_back(node->val);
+        getNumbers(node->right);
+    }
+    TreeNode* balanceTree(int l, int r) {
+        if (l > r) return nullptr;
+        int middleIdx = l + (r - l) / 2;
+        TreeNode* root = new TreeNode(nums[middleIdx]);
+        root->left = balanceTree(l, middleIdx - 1);
+        root->right = balanceTree(middleIdx + 1, r);
+        return root;
     }
 };
