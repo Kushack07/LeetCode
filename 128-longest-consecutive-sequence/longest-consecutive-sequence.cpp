a3015a3cbb8ff3b0798c1d239b632f425da04216
KUSH <kush18092004@gmail.com>
2026-02-09 19:46:33 +0530

    Time: 95 ms (20.37%) | Memory: 89 MB (23.66%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -3,15 +3,42 @@ public:
 int x=1,y=1;
     int longestConsecutive(vector<int>& a) {
         if(a.empty()) return 0;
-        sort(a.begin(),a.end());
-        for(int i=1;i<a.size();i++)
+       // sort(a.begin(),a.end());
+       
+    //     for(int i=1;i<a.size();i++)
+    //     {
+    //         if(a[i]==a[i-1]) continue;
+    //         if(a[i]==a[i-1]+1)x++;
+    //         else{
+    //             y=max(y,x);x=1;
+    //         }
+    //       }
+    // return max(x,y);
+//--------------------O(N)--------------------------
+unordered_set<int>st;
+for(int i:a) st.insert(i);
+int res=0;
+for(int i:a)
+{
+    if(st.find(i)!=st.end())
+    {
+        if(st.find(i-1)==st.end())
         {
-            if(a[i]==a[i-1]) continue;
-            if(a[i]==a[i-1]+1)x++;
-            else{
-                y=max(y,x);x=1;
+            int cur=i,cnt=0;
+            while(st.find(cur)!=st.end())
+            {
+                st.erase(cur);
+                cur++;
+                cnt++;
             }
-          }
-    return max(x,y);
+        
+        res=max(res,cnt);
+    }
+    }
+}
+return res;
+
+
+
     }
 };
