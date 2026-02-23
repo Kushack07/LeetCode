41caba664e2ae6fac841d58e10f0f40318b427f7
KUSH <kush18092004@gmail.com>
2026-02-23 20:06:20 +0530

    Time: 395 ms (63.58%) | Memory: 119.1 MB (59.96%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,24 +1,10 @@
 class Solution {
 public:
     bool hasAllCodes(string s, int k) {
-        int n=s.size();
-        if(n<k) return false;
-        int need=pow(2,k);//s^k
-        int m=need-1;
-        int h=0;
-        unordered_set<int>st;
-        for(int i=0;i<n;i++)
-        {
-            h=((h<<1)&m)|(s[i]-'0');
-            if(i>=k-1)
-            {
-            st.insert(h);
-            }
-            if(st.size()==need)
-            {
-                return true;
-            }
+        unordered_set<string>st;
+        for(int i=0 ;i+k <=s.size();i++){
+            st.insert(s.substr(i,k));
         }
-        return false;
+        return st.size()==(int)pow(2,k);
     }
 };
