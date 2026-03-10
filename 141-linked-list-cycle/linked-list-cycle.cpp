cf96b386825052fba2616796b8b4659d5d7d96c5
KUSH <kush18092004@gmail.com>
2026-03-10 19:44:17 +0530

    Time: 12 ms (42.85%) | Memory: 11.8 MB (53.22%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -1,22 +1,18 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(!head || !head->next)return false;
-        ListNode*f = head; 
-        ListNode*s = head; 
-        while(f->next && f->next->next){
-            f=f->next->next;
-            s=s->next;
-            if(f==s)return true;
+        ListNode* fast = head;
+        ListNode* slow = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            fast = fast->next->next;
+            slow = slow->next;
+
+            if (fast == slow) {
+                return true;
+            }
         }
-        return false; 
+
+        return false;        
     }
 };
