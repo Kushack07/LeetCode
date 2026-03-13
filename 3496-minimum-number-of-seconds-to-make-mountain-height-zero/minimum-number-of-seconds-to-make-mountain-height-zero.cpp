145a6bff15f8a414318eb99df502f3c1207f9025
KUSH <kush18092004@gmail.com>
2026-03-13 23:02:14 +0530

    Time: 26 ms (58.06%) | Memory: 29 MB (65.81%) - LeetSync

--- 3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.cpp
+++ 3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    typedef long long ll;
-    long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
-        //approach 1
-        priority_queue<vector<ll>,vector<vector<ll>>,greater<vector<ll>>>pq;
-        // next_free_time base_time piles_eaten
-        for(auto it:workerTimes){
-            pq.push({it,it,1});
+    inline static long long f(long long t, vector<int>& workerTimes){
+        long long x=0;
+        for(int w: workerTimes)
+            x+=(long long)((sqrt(1+8.0*t/w)-1)/2);
+        return x;
+    }
+    static long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
+        int wz=workerTimes.size();
+        if (wz==1)
+            return (long long)workerTimes[0]*mountainHeight*(mountainHeight+1)/2;
+        long long l=1, r=1e12*(long long)mountainHeight/wz;
+        while(l<r){
+            long long m=l+(r-l)/2;
+            long long x=f(m, workerTimes);
+            if(x>=mountainHeight) r=m;
+            else l=m+1;
         }
-        ll ans=0,currHeight=mountainHeight;
-        while(!pq.empty()){
-            auto it=pq.top();
-            pq.pop();
-            ll maxTimeHeWorked=it[0];
-            ll baseTime=it[1];
-            ll reduced=it[2];
-            ans=max(ans,maxTimeHeWorked);
-            currHeight--;
-            if(currHeight==0)break;
-            ll nextheight=reduced+1;
-            ll totalTimeToBreak=(baseTime*(nextheight)*(nextheight+1))/2;
-            pq.push({totalTimeToBreak,baseTime,nextheight});
-        }
-        return ans;
+        return l;
     }
 };
