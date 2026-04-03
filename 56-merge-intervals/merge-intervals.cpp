e63aa9674588ba97ee082bdb2d57d08fafdbb315
KUSH <kush18092004@gmail.com>
2026-04-03 19:21:31 +0530

    Time: 6 ms (46.69%) | Memory: 23.8 MB (80.91%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& ik) {
-
-        vector<vector<int>> res;
-
-        sort(ik.begin(), ik.end());
-
-        int start = ik[0][0];
+        sort(ik.begin(),ik.end());
+        vector<vector<int>>res;
+        int start=ik[0][0];
         int end = ik[0][1];
-
-        for(int i = 1; i < ik.size(); i++) {
-
-            int start2 = ik[i][0];
-            int end2 = ik[i][1];
-
-            if(start2 <= end) {
-                end = max(end, end2);
-            }
-            else {
-                res.push_back({start, end});
-                start = start2;
-                end = end2;
+        for(int i =0 ;i<ik.size();i++){
+            int start1=ik[i][0];
+            int end1 =ik[i][1];
+            if(start1<=end){
+                end = max(end,end1);
+            }else{
+                res.push_back({start,end});
+                start =start1;
+                end = end1;
             }
         }
-
-        res.push_back({start, end});
-
+        res.push_back({start,end});
         return res;
     }
-}; 
+};
