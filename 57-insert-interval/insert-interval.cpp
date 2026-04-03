4e86162dc1980a0be426adf998448ef8d07b6450
KUSH <kush18092004@gmail.com>
2026-04-03 21:46:27 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (46.27%) - LeetSync

---
+++ 57-insert-interval/insert-interval.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        
+        vector<vector<int>> res;
+        int i = 0;
+        int n = intervals.size();
+
+        // Step 1: add intervals before newInterval
+        while(i < n && intervals[i][1] < newInterval[0]) {
+            res.push_back(intervals[i]);
+            i++;
+        }
+
+        // Step 2: merge overlapping intervals
+        while(i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+
+        res.push_back(newInterval);
+
+        // Step 3: add remaining intervals
+        while(i < n) {
+            res.push_back(intervals[i]);
+            i++;
+        }
+
+        return res;
+    }
+};
