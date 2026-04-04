7351619d070559bd48325d6f2806fd6603c01236
KUSH <kush18092004@gmail.com>
2026-04-04 23:46:23 +0530

    Time: 1 ms (55.85%) | Memory: 23 MB (26.89%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.cpp
+++ 1028-interval-list-intersections/interval-list-intersections.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> intervalIntersection(vector<vector<int>>& fl, vector<vector<int>>& sl) {
-        int n =fl.size();
-        int m =sl.size();
         vector<vector<int>>res;
         int i=0,j=0;
+        int n =fl.size();
+        int m =sl.size();
         while(i<n && j<m){
-            int start=max(fl[i][0],sl[j][0]);
-            int end =min(fl[i][1],sl[j][1]);
+            int start = max(fl[i][0],sl[j][0]);
+            int end = min(fl[i][1],sl[j][1]);
             if(start<=end){
                 res.push_back({start,end});
             }
             if(fl[i][1]<sl[j][1])
                 i++;
-            else
+            else 
                 j++;
         }
         return res;
