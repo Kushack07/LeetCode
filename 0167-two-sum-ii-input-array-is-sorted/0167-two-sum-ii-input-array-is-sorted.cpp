3bdb0ebcf47c2a6da56ae0dd1eb272df1302a622
KUSH <kush18092004@gmail.com>
2026-04-04 23:59:04 +0530

    Time: 0 ms (100%), Space: 19.6 MB (32.95%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>&a, int t) {
-       unordered_map<int,int>mp;
-       for(int i=0;i<a.size();i++)
-       {
-        int comp=t-a[i];
-        if(mp.find(comp)!=mp.end())
-        {
-            return {mp[comp]+1,i+1};
+    vector<int> twoSum(vector<int>& a, int t) {
+        const int OFFSET = 10000;
+        int hash[20001] = {0};
+
+        for(int i = 0; i < a.size(); i++) {
+            int comp = t - a[i];
+            if(comp >= -10000 && comp <= 10000 && hash[comp + OFFSET] != 0) {
+                return {hash[comp + OFFSET] , i+1};
+            }
+            hash[a[i] + OFFSET] = i + 1;
         }
-        mp[a[i]]=i;
-
-       }
-       return {};
+        return {};
     }
-
 };
