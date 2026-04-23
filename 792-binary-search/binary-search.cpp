46dde95999f4c365f7b7eb1962cce25dfe876e0c
KUSH <kush18092004@gmail.com>
2026-04-24 00:59:44 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (7.57%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
-        int n = nums.size();
-        for(int i=0;i<n;i++){
-            if(nums[i]==target){
-                return i ;
+        int left = 0;
+        int right = nums.size() - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-        return -1; 
+        
+        return -1;
     }
 };
