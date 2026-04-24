fcce8e3a9f5e65074e9786da166bc6a7453c4277
KUSH <kush18092004@gmail.com>
2026-04-24 08:43:59 +0530

    Time: 3 ms (39.79%) | Memory: 8.5 MB (31.81%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        string result = "";
-        int i =0 ; 
-        while(i<word1.length()||i<word2.length()){
-            if(i<word1.length()){
-                result += word1[i];
+        
+        int n = word1.size();
+        int m = word2.size();
+        
+        string word3;
+        
+        int i = 0, j = 0;
+        
+        while(i < n || j < m){
+            
+            if(i < n){
+                word3.push_back(word1[i]);
+                i++;
             }
-            if(i<word2.length()){
-                result += word2[i];
+            
+            if(j < m){
+                word3.push_back(word2[j]);
+                j++;
             }
-            i++;
         }
-        return result;
+        
+        return word3;
     }
 };
