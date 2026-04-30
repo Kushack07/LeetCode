366a54decdcc5820a3f8e8c699ca3f9788b57aca
KUSH <kush18092004@gmail.com>
2026-04-30 22:23:15 +0530

    Time: 8163 ms (54.41%), Space: 125.6 MB (13.99%) - LeetHub

---
+++ 3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxPathScore(self, g: List[List[int]], k: int) -> int:
+        @cache
+        def dfs(i: int, j: int, k: int) -> int:
+            if i < 0 or j < 0 or k < 0:
+                return -inf
+            if i == 0 and j == 0:
+                return 0
+            res = g[i][j]
+            if g[i][j]:
+                k -= 1
+            a = dfs(i - 1, j, k)
+            b = dfs(i, j - 1, k)
+            res += max(a, b)
+            return res
+        ans = dfs(len(g) - 1, len(g[0]) - 1, k)
+        dfs.cache_clear()
+        return -1 if ans < 0 else ans
