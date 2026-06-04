bf470af4c296672d2795f7eb4ddcc3f90afb144c
KUSH <kush18092004@gmail.com>
2026-06-04 14:48:20 +0530

    Time: 1 ms (86.98%), Space: 11.3 MB (64.27%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
 typedef long long ll;
+
+int f2(int n){
+    vector<int>a(n);a[0]=1;
+    int i2,i3,i5,m2,m3,m5;
+    i2=i3=i5=0;
+    m2=2,m3=3,m5=5;
+    for(int i=1;i<n;i++)
+    {
+        int cur=min(m2,min(m3,m5));
+        a[i]=cur;
+        if(cur==m2) m2=a[++i2]*2;
+        if(cur==m3) m3=a[++i3]*3;
+        if(cur==m5) m5=a[++i5]*5;
+    }
+    return a[n-1];
+}
 int f(int x)
 {
     priority_queue<ll,vector<ll>,greater<ll>>q;
@@ -21,6 +37,6 @@ int f(int x)
     return (int)res;
 }
     int nthUglyNumber(int n) {
-        return f(n);
+        return f2(n);
     }
 };
