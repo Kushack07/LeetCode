1879240d138ca8c55c8598b932b5dde728837512
KUSH <kush18092004@gmail.com>
2026-06-13 07:06:11 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (83.43%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,24 +1,14 @@
-class Solution {
-public:
-    int removeElement(vector<int>& a, int v) {
-        int n = a.size();
-        int k = 0;
-        for(int i=0;i<n;i++){
-            if(a[i]!=v){
-                a[k]=a[i];//a[k]=333333
-                k++;
+    class Solution {
+    public:
+        int removeElement(vector<int>& nums, int val) {
+            int n = nums.size();
+            int k =0;
+            for(int i=0;i<n;i++){
+                if(nums[i]!=val){
+                    nums[k]=nums[i];
+                    k++;
+                }
             }
+            return k;
         }
-        return k;
-    }
-};
-/*
-Example 1:
-
-Input: nums = [3,2,2,3], val = 3
-Output: 2, nums = [2,2,_,_]
-val=2
-nums
-Explanation: Your function should return k = 2, with the first two elements of nums being 2.
-It does not matter what you leave beyond the returned k (hence they are underscores).
-*/
+    };
