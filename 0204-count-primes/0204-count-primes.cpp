2e8fc79e87f8736404969d0e271b3efc10ee4a7e
KUSH <kush18092004@gmail.com>
2026-06-15 18:43:11 +0530

    Time: 384 ms (7.53%), Space: 12.7 MB (63.42%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<bool>res(n,true);
+        if(n<=2) return 0;
+        res[0]=res[1]=false;
+        for(int i=2;i*i<n;i++)
+        {
+            for(int j=i*i;j<n;j+=i)
+            {
+                res[j]=false;
+            }
+        }
+        return count(res.begin(),res.end(),true);
+    }
+};
