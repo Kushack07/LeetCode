20deb8fa402ee60f2d23308154a080a6556e81bd
KUSH <kush18092004@gmail.com>
2026-06-15 16:21:16 +0530

    Time: 3 ms (43.33%), Space: 32.9 MB (99.76%) - LeetHub

--- 1250-check-if-it-is-a-good-array/1250-check-if-it-is-a-good-array.cpp
+++ 1250-check-if-it-is-a-good-array/1250-check-if-it-is-a-good-array.cpp
@@ -1,8 +1,13 @@
 class Solution {
 public:
-    bool isGoodArray(vector<int>& a) {
-        auto gcd=a[0];
-        for(auto i:a) gcd=__gcd(gcd,i);
-        return gcd==1;
+    bool isGoodArray(vector<int>&a) {
+        int g=0;
+        for(int i=0;i<a.size();i++){
+            g=gcd(g,a[i]);
+            if(g==1){
+                return true;
+            }
+        }
+        return false;
     }
 };
