d99ab7a0ee670892dd4622540a60ba43a0d9047b
KUSH <kush18092004@gmail.com>
2026-06-15 09:33:16 +0530

    Time: 0 ms (100%), Space: 24.1 MB (7.08%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -1,48 +1,7 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
 typedef ListNode l;
-// l* copy(vector<int>&a)
-// {
-//     if(a.empty()) return nullptr;
-//     l* head=new l(a[0]);
-//     l* cur=head;
-//     for(int i=1;i<a.size();i++)
-//     {
-//         cur->next=new l(a[i]);
-//         cur=cur->next;
-//     }
-//     return head;
-// }
     void reorderList(ListNode* head) {
-//         l* cur=head;vector<int>a;
-//         while(cur){
-//             a.push_back(cur->val);
-//             cur=cur->next;
-//         }
-//    //     for(int i:a) cout<<i<<" ";
-//         int n=a.size();
-//         vector<int>ans;
-//         int f=a[0],l=a[n-1],x=a[1];
-//         ans.push_back(f);
-//         ans.push_back(l);
-//         ans.push_back(x);
-//         for(int i=2;i<n-1;i++)
-//         {
-//             ans.push_back(a[i]);
-//         }
-//         for(int i:ans) cout<<i<<" ";
-//         head=copy(ans);
-//       //  l* res=copy(ans);
 if(!head || !head->next) return ;
 l* cur=head;
 vector<int>a;
