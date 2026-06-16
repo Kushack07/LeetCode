896e6881f7e09cb919d4888b76260b690a8822d9
KUSH <kush18092004@gmail.com>
2026-06-16 08:14:03 +0530

    Time: 4 ms (63.64%) | Memory: 39.8 MB (69.70%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     string processStr(string s) {
-        int n=s.size();
-        string res;
-        for(char ch : s){
-            if(ch=='*'){
+        string res ; 
+        for(char ch :s){
+            if(ch == '*'){
                 if(!res.empty())
                     res.pop_back();
-            }else if(ch =='%'){
+            }
+            else if(ch =='#'){
+                res+=res; 
+            }
+            else if(ch == '%'){
                 reverse(res.begin(),res.end());
-            }else if(ch =='#'){
-                res +=res;  
             }else{
                 res.push_back(ch);
             }
         }
-        return res;
+        return res; 
     }
 };
