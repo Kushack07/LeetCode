a0bafc98e6307f1763cb95907fc782bdd5cd9455
KUSH <kush18092004@gmail.com>
2026-06-19 08:05:38 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (44.68%) - LeetSync

---
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int alt =  0;
+        int heigh = 0;
+        for(int x:gain){
+            alt += x; 
+            heigh = max(heigh,alt);
+        }
+        return heigh ;
+    }
+};
