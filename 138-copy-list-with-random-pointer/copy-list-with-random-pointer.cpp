2c39e31ce1c3452aabeca0aaac8faebff3ca1ef3
KUSH <kush18092004@gmail.com>
2026-06-20 10:20:59 +0530

    Time: 11 ms (14.61%) | Memory: 15.3 MB (8.58%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -1,35 +1,23 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* next;
-    Node* random;
-    
-    Node(int _val) {
-        val = _val;
-        next = NULL;
-        random = NULL;
-    }
-};
-*/
-
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        if(!head)return NULL;
-        unordered_map<Node*,Node*>mp;
-        Node*temp = head;
-        while(temp){
-            mp[temp] =new Node(temp->val);
-            temp = temp->next;
+        if (!head) return nullptr;
+        
+        unordered_map<Node*, Node*> old_to_new;
+        
+        Node* curr = head;
+        while (curr) {
+            old_to_new[curr] = new Node(curr->val);
+            curr = curr->next;
         }
-        temp = head;
-        while(temp){
-            mp[temp]->next= mp[temp->next];
-            mp[temp]->random=mp[temp->random];
-            temp = temp->next;
+        
+        curr = head;
+        while (curr) {
+            old_to_new[curr]->next = old_to_new[curr->next];
+            old_to_new[curr]->random = old_to_new[curr->random];
+            curr = curr->next;
         }
-        return mp[head];
+        
+        return old_to_new[head];
     }
 };
