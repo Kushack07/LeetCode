50ef90671395556a3b59fe0a59934b2f090cbd1e
KUSH <kush18092004@gmail.com>
2026-06-21 07:38:29 +0530

    Time: 38 ms (33.97%) | Memory: 80.4 MB (37.51%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
-        int n = costs.size();
-        int cnt =0; 
-        int sum =0; 
-        sort(costs.begin(),costs.end());
-        for(int i=0;i<n;i++){
-            if(coins>=costs[i]){
-                coins-=costs[i];
+        sort(costs.begin(), costs.end());
+
+        int cnt = 0;
+
+        for(int cost : costs) {
+            if(coins >= cost) {
+                coins -= cost;
                 cnt++;
-            }else{
+            } else {
                 break;
             }
-          
-            }
-        
+        }
+
         return cnt;
     }
 };
