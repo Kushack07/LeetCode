c13adaa28c6dbb19a890df9c99d6c24e92c3f419
KUSH <kush18092004@gmail.com>
2026-06-22 09:05:35 +0530

    Time: 0 ms (100%), Space: 8.8 MB (95.38%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -12,7 +12,7 @@ public:
     //     }
     //     l=l/2,o=o/2;
     // return min({b,a,l,o,n});
-    map<char,int>f;
+map<char,int>f;
     for(char c:s) f[c]++;
     return min({f['b'],f['a'],f['l']/2,f['n'],f['o']/2});
     }
