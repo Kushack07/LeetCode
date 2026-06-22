2b51a6e416fe6f55a3b64f938c1cbe928232a8fa
KUSH <kush18092004@gmail.com>
2026-06-22 08:51:22 +0530

    Time: 5 ms (0.65%), Space: 9 MB (39.61%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int maxNumberOfBalloons(string s) {
-int b=0,a=0,l=0,n=0,o=0;
-       for(char c:s)
-        {
-            if(c=='b')b++;
-            if(c=='a')a++;
-            if(c=='l')l++;
-            if(c=='n')n++;
-            if(c=='o')o++;
-        }
-        l=l/2,o=o/2;
-    return min({b,a,l,o,n});
+    // int b=0,a=0,l=0,n=0,o=0;
+    //    for(char c:s)
+    //     {
+    //         if(c=='b')b++;
+    //         if(c=='a')a++;
+    //         if(c=='l')l++;
+    //         if(c=='n')n++;
+    //         if(c=='o')o++;
+    //     }
+    //     l=l/2,o=o/2;
+    // return min({b,a,l,o,n});
+    map<char,int>f;
+    for(char c:s) f[c]++;
+    return min({f['b'],f['a'],f['l']/2,f['n'],f['o']/2});
     }
 };
