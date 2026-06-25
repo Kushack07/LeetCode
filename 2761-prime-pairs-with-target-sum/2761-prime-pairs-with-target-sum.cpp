4532fddeb1d4e067f851ecf2e56ee1a3a52330d2
KUSH <kush18092004@gmail.com>
2026-06-25 23:40:59 +0530

    Time: 1925 ms (9.05%), Space: 30.7 MB (99.76%) - LeetHub

--- 2761-prime-pairs-with-target-sum/2761-prime-pairs-with-target-sum.cpp
+++ 2761-prime-pairs-with-target-sum/2761-prime-pairs-with-target-sum.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
 typedef vector<vector<int>> vii;
-static const int M=1e6;
-vector<bool>prime;
-Solution():prime(M+1,true){
-    sieve();
-}
-void sieve()
+bool f(int n)
 {
-    prime[0]=prime[1]=false;
-    for(int i=2;i*i<=M;i++)
+    if(n<2) return false;
+    for(int i=2;i*i<=n;i++)
     {
-        if(prime[i])
-        {
-            for(int j=i*i;j<=M;j+=i)
-            {
-                prime[j]=false;
-            }
-        }
+        if(n%i==0) return false;
     }
+    return true;
 }
     vector<vector<int>> findPrimePairs(int n) {
         vii ans;
-        for(int i=2;i<=n/2;i++)
+        for(int i=0;i<=n/2;i++)
         {
-            if(prime[i] && prime[n-i])
-            {
-                ans.push_back({i,n-i});
-            }
+            if(f(i) && f(n-i)) ans.push_back({i,n-i});
         }
         return ans;
     }
