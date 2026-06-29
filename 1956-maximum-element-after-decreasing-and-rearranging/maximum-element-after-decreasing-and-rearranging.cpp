46c46b1566287d6b0a6181b5ce84130c1f469061
KUSH <kush18092004@gmail.com>
2026-06-29 12:25:08 +0530

    Time: 0 ms (100.00%) | Memory: 55.7 MB (4.48%) - LeetSync

--- 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
@@ -1,20 +1,22 @@
-//translated using AI
 class Solution {
 public:
-    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
-        int n = arr.size();
-        vector<int> cnt(n + 1, 0);
-
-        for (int x : arr) {
-            cnt[min(x, n)]++;
+    using u32 = uint32_t;
+    static constexpr u32 m = 100'000;
+    inline static u32 f[m + 1];
+    u32 maximumElementAfterDecrementingAndRearranging(
+        const vector<int>& nums) noexcept {
+        u32 a = m, b = 0, v = 0;
+        for (u32 x : nums) {
+            x = min(x, m);
+            a = min(a, x);
+            b = max(b, x);
+            ++f[x];
         }
-
-        int val = 0;
-
-        for (int i = 1; i <= n; i++) {
-            val = min(i, val + cnt[i]);
+        [[assume(b <= m)]];
+        while (a <= b) {
+            v += min(a - v, f[a]);
+            f[a++] = 0;
         }
-
-        return val;
+        return v;
     }
 };
