d8fd61a83acf5a684d46921049b2e2657b09292a
KUSH <kush18092004@gmail.com>
2026-06-29 12:29:51 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (33.74%) - LeetSync

--- 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.cpp
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int numOfStrings(vector<string>& pat, string word) {
-        int cnt =0 ;
-        for(string s : pat){
-            if(word.find(s) !=string::npos){
+        int cnt = 0; 
+        for(string c : pat){
+            if(word.find(c) !=string::npos)
                 cnt++;
-            }
         }
         return cnt;
     }
