8d9432445454c46492b9e174da53f700832d139a
KUSH <kush18092004@gmail.com>
2026-07-14 01:12:23 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (82.53%) - LeetSync

--- 2561-number-of-distinct-averages/number-of-distinct-averages.cpp
+++ 2561-number-of-distinct-averages/number-of-distinct-averages.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int distinctAverages(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        set<long long >st;
+        set<double>st;
         int i=0 ; 
         int j=nums.size()-1;
         while(i<j){
-            st.insert(nums[i]+nums[j]);
+            st.insert((nums[i]+nums[j])/2.0);
             i++;
             j--;
         }
