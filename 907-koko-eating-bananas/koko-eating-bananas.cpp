2cc2b4ae450ff6eb524d5d8505d8b3d37b8f9df6
KUSH <kush18092004@gmail.com>
2026-07-14 22:02:30 +0530

    Time: 8 ms (55.04%) | Memory: 23.1 MB (15.04%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,31 +1,71 @@
 class Solution {
 public:
-typedef long long ll;
-bool ok(vector<int>&a,int k,int m)
-{
-    ll ans=0;
-    for(int i=0;i<a.size();i++)
-        ans+=(a[i]+m-1)/m;
-    return ans<=k;
-}
-int bs(vector<int>&a,int k)
-{
-    int l=1,h=*max_element(a.begin(),a.end()),res=0;
-    while(l<=h)
-    {
-        int m=l+(h-l)/2;
-        if(ok(a,k,m))
-        {
-            h=m-1;
-            res=m;
+
+    // This function checks whether Koko can finish
+    // all the bananas with the given eating speed.
+    bool canFinish(vector<int>& piles, int h, int speed) {
+
+        // Total hours required at this speed
+        long long hours = 0;
+
+        // Visit every pile
+        for (int pile : piles) {
+
+            // Hours needed for this pile
+            // ceil(pile / speed)
+            //
+            // Example:
+            // pile = 7
+            // speed = 4
+            //
+            // 7/4 = 1.75
+            // Need 2 hours
+            //
+            // Formula:
+            // ceil(a/b) = (a+b-1)/b
+            hours += (pile + speed - 1) / speed;
         }
-        else{
-            l=m+1;
-        }
+
+        // If total hours are within h,
+        // then this speed is sufficient.
+        return hours <= h;
     }
-    return res;
-}
-    int minEatingSpeed(vector<int>& a, int h) {
-        return bs(a,h);
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+
+        // Minimum possible eating speed
+        int low = 1;
+
+        // Maximum possible eating speed
+        // (largest pile)
+        int high = *max_element(piles.begin(), piles.end());
+
+        // Store answer
+        int ans = high;
+
+        // Binary Search on answer
+        while (low <= high) {
+
+            // Middle speed
+            int mid = low + (high - low) / 2;
+
+            // Check if Koko can finish
+            if (canFinish(piles, h, mid)) {
+
+                // Current speed works
+                ans = mid;
+
+                // Try to reduce speed
+                high = mid - 1;
+            }
+            else {
+
+                // Current speed is too slow
+                // Increase speed
+                low = mid + 1;
+            }
+        }
+
+        return ans;
     }
 };
