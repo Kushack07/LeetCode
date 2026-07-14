8e8c582530f5c9cf7c86f93ec23c9d0f38972a78
KUSH <kush18092004@gmail.com>
2026-07-14 10:16:57 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (31.23%) - LeetSync

---
+++ 670-maximum-swap/maximum-swap.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maximumSwap(int num) {
+        string s = to_string(num);/*convert the num to string*/
+
+        vector<int> last(10); /*covert the vector in range of 9 */
+
+        for (int i = 0; i < s.size(); i++) {
+            last[s[i] - '0'] = i; /*store only the non zero value*/
+        }
+
+        for (int i = 0; i < s.size(); i++) {/*run the from 0 to s.size()*/
+            for (int d = 9; d > s[i] - '0'; d--) {/*d from 9 to  non negative number in the s[i] string*/
+                if (last[d] > i) {
+                    swap(s[i], s[last[d]]);
+                    return stoi(s);
+                }
+            }
+        }
+
+        return num;
+    }
+};
