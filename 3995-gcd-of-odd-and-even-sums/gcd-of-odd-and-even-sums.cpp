cb3d327eb7ca140fbe825134dfc28c54671c12a4
KUSH <kush18092004@gmail.com>
2026-07-15 17:51:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (46.67%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -1,7 +1,13 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        return n ;
-        
+        int sume =0; 
+        int sumo =0;
+        if(n%2!=0){
+            sumo+=n;
+        }else{
+            sume+=n; 
+        }
+        return std::gcd(sume,sumo);
     }
 };
