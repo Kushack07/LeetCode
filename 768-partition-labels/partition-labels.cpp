f11666c69bea6de10dc1ba28c6a7fb4b988414e4
KUSH <kush18092004@gmail.com>
2026-07-15 19:29:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (66.18%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -2,28 +2,20 @@ class Solution {
 public:
     vector<int> partitionLabels(string s) {
         int n = s.size();
-
-        vector<int> last(26);
-
-        // Store last occurrence of every character
-        for (int i = 0; i < n; i++) {
-            last[s[i] - 'a'] = i;
+        vector<int>last(26); 
+        for(int i=0;i<n;i++){
+            last[s[i]-'a']=i;
         }
-
-        vector<int> ans;
-        int start = 0;
-        int end = 0;
-
-        for (int i = 0; i < n; i++) {
-            end = max(end, last[s[i] - 'a']);
-
-            // Partition completed
-            if (i == end) {
-                ans.push_back(end - start + 1);
-                start = i + 1;
+        int start =0; 
+        int end=0; 
+        vector<int>ans ; 
+        for (int i=0;i<n;i++){
+            end = max(end,last[s[i]-'a']);
+            if(i==end){
+                ans.push_back(end-start+1);
+                start =i+1;
             }
         }
-
-        return ans;
+        return ans ;
     }
 };
