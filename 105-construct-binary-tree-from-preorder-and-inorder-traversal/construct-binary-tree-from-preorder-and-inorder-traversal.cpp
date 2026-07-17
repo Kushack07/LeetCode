9901d461cb0603c0baa5678c167b5984dd0088eb
KUSH <kush18092004@gmail.com>
2026-07-17 12:13:19 +0530

    Time: 29 ms (5.66%) | Memory: 74.6 MB (5.82%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    int preorderIndex = 0;
-    unordered_map<int, int> inorderMap;
-
-    TreeNode* build(int left, int right, vector<int>& preorder) {
-
-        if (left > right)
-            return nullptr;
-
-        int rootValue = preorder[preorderIndex++];
-        TreeNode* root = new TreeNode(rootValue);
-
-        int inorderIndex = inorderMap[rootValue];
-
-        root->left = build(left, inorderIndex - 1, preorder);
-        root->right = build(inorderIndex + 1, right, preorder);
-
-        return root;
-    }
-
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
 
-        for (int i = 0; i < inorder.size(); i++) {
-            inorderMap[inorder[i]] = i;
-        }
+        if (preorder.empty())
+            return nullptr;
 
-        return build(0, inorder.size() - 1, preorder);
+        // Root is always first element of preorder
+        TreeNode* root = new TreeNode(preorder[0]);
+
+        // Find root in inorder
+        int idx = 0;
+        while (inorder[idx] != preorder[0])
+            idx++;
+
+        // Left subtree
+        vector<int> leftInorder(inorder.begin(), inorder.begin() + idx);
+        vector<int> leftPreorder(preorder.begin() + 1,
+                                 preorder.begin() + 1 + idx);
+
+        // Right subtree
+        vector<int> rightInorder(inorder.begin() + idx + 1, inorder.end());
+        vector<int> rightPreorder(preorder.begin() + 1 + idx,
+                                  preorder.end());
+
+        root->left = buildTree(leftPreorder, leftInorder);
+        root->right = buildTree(rightPreorder, rightInorder);
+
+        return root;
     }
 };
