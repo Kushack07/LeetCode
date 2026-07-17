0d1dbd54fdad3dfc14b514ad8a574ffd5b5d1e76
KUSH <kush18092004@gmail.com>
2026-07-17 10:08:52 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (48.13%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -16,11 +16,11 @@ public:
             return true;
         }
         if(p==nullptr || q==nullptr){
-            return false;
+            return false ;
         }
-        if(p->val !=q->val){
-            return false;
+        if(p->val != q->val){
+            return false; 
         }
-        return isSameTree(p->left,q->left)&&isSameTree(p->right,q->right);
+        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
     }
 };
