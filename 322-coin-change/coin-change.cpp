e3a906d348ebe96601c7bce7c42243f8290f0d9f
KUSH <kush18092004@gmail.com>
2026-07-17 23:31:40 +0530

    Time: 39 ms (48.31%) | Memory: 52.9 MB (14.17%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -5,8 +5,7 @@ public:
         int n = coins.size();
         int INF = 1e9;
 
-        vector<vector<int>> dp(n + 1,
-                               vector<int>(amount + 1, INF));
+        vector<vector<int>> dp(n + 1,vector<int>(amount + 1, INF));
 
         for (int i = 0; i <= n; i++) {
             dp[i][0] = 0;
@@ -16,16 +15,16 @@ public:
             for (int j = 1; j <= amount; j++) {
 
                 if (coins[i - 1] <= j) {
-                    dp[i][j] = min(
-                        1 + dp[i][j - coins[i - 1]],
-                        dp[i - 1][j]
-                    );
+                    dp[i][j] = min(1 + dp[i][j - coins[i - 1]],dp[i - 1][j]);
                 } else {
                     dp[i][j] = dp[i - 1][j];
                 }
             }
         }
-
-        return dp[n][amount] == INF ? -1 : dp[n][amount];
+        if(dp[n][amount]==INF){
+            return -1; 
+        }else{
+            return dp[n][amount];
+        }
     }
 };
