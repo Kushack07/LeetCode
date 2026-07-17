622159fcbedffcf69f6391c8b01109595384d044
KUSH <kush18092004@gmail.com>
2026-07-17 23:23:30 +0530

    Time: 43 ms (43.11%) | Memory: 52.8 MB (14.48%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount + 1, amount + 1);
 
-        dp[0] = 0;
+        int n = coins.size();
+        int INF = 1e9;
 
-        for(int i = 1; i <= amount; i++){
-            for(int coin : coins){
-                if(i - coin >= 0){
-                    dp[i] = min(dp[i], dp[i - coin] + 1);
+        vector<vector<int>> dp(n + 1,
+                               vector<int>(amount + 1, INF));
+
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 0;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= amount; j++) {
+
+                if (coins[i - 1] <= j) {
+                    dp[i][j] = min(
+                        1 + dp[i][j - coins[i - 1]],
+                        dp[i - 1][j]
+                    );
+                } else {
+                    dp[i][j] = dp[i - 1][j];
                 }
             }
         }
 
-        if(dp[amount] > amount) return -1;
-
-        return dp[amount];
+        return dp[n][amount] == INF ? -1 : dp[n][amount];
     }
 };
