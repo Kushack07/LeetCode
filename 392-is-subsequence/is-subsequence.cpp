004c8307718259c9001ec0008a691a34935f428a
KUSH <kush18092004@gmail.com>
2026-07-19 16:34:56 +0530

    Time: 3 ms (4.23%) | Memory: 10.3 MB (5.17%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int i =0 ; int j= 0 ;
-        while(i < s.size() && j<t.size()){
-            if(s[i]==t[j]){
-                i++;
+        int n = s.size();
+        int m = t.size();
+
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s[i - 1] == t[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
             }
-            j++;
         }
-        return i==s.size();
+
+        return dp[n][m] == n;
     }
 };
