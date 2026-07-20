ec0698405e9e685a6a106a87f3fecc505d9c4107
KUSH <kush18092004@gmail.com>
2026-07-20 22:18:00 +0530

    Time: 2 ms (14.89%) | Memory: 8.9 MB (38.22%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -2,20 +2,22 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char>st;
-        for(char ch:s){ 
-            if(ch == '(' || ch =='{' || ch == '['){
-                st.push(ch);
+        for(char c :s ){
+            if(c=='(' || c=='{' || c== '['){
+                st.push(c);
             }else{
-                if(st.empty())return false;
-                char top  = st.top();
-                st.pop();
-                if((ch==')' && top!= '(')||
-                    (ch=='}' && top!='{')||
-                    (ch==']' && top!='[')){
-                        return false; 
-                    }
-            }
+            if(st.empty())
+                return false;
+            char top = st.top();
+            st.pop();
+            if((c==')'&& top != '(')||
+                (c=='}'&& top!='{')||
+                (c==']' && top!= '[')){
+                    return false ;
+                }
         }
+    }
         return st.empty();
+
     }
 };
