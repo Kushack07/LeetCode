26796168528599760b00e5e854b680fe7d0ecf93
KUSH <kush18092004@gmail.com>
2026-07-23 01:29:23 +0530

    Time: 138 ms (45.84%) | Memory: 17.3 MB (65.77%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    bool subsetsum(vector<int>&arr,int sum){
-        int n =arr.size();
+    bool partion(vector<int>&ans,int sum){
+        int n =ans.size();
         vector<vector<bool>>dp(n+1,vector<bool>(sum+1,false));
-        //sum is 0 always possible 
         for(int i=0;i<=n;i++){
             dp[i][0]=true;
         }
         for(int i=1;i<=n;i++){
             for(int j=1;j<=sum;j++){
-                // current element 
-                if(arr[i-1]<=j){
-                    dp[i][j]=dp[i-1][j-arr[i-1]]||dp[i-1][j];
-                }
-                else{
-                    dp[i][j]=dp[i-1][j];
+                //take 
+                if(ans[i-1]<=j){
+                    dp[i][j]=dp[i-1][j-ans[i-1]]||dp[i-1][j]; //take the value 
+                }else{
+                    dp[i][j]=dp[i-1][j];//skip 
                 }
             }
         }
         return dp[n][sum];
     }
     bool canPartition(vector<int>& nums) {
-        int totalsum =0 ;
-        for(int x:nums){
-            totalsum += x; 
+        int n= nums.size();
+        int ts =0 ;
+        for(int i :nums){
+            ts +=i ;
         }
-        if(totalsum%2!=0){
+        if(ts%2!=0){
             return false;
         }
-        return subsetsum(nums,totalsum/2);
+        return partion(nums,(ts/2));
     }
 };
