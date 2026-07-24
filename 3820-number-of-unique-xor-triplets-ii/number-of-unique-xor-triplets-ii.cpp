ced73b06688197a44858df6d2aa468ad3f7675fa
KUSH <kush18092004@gmail.com>
2026-07-24 21:25:53 +0530

    Time: 2056 ms (37.64%) | Memory: 98.5 MB (15.73%) - LeetSync

--- 3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.cpp
+++ 3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
-        unordered_set<int> twoXor;
-        unordered_set<int> ans;
-
         int n = nums.size();
-
-        // All possible XORs of two elements
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                twoXor.insert(nums[i] ^ nums[j]);
+        unordered_map<int,int>ans;
+        unordered_map<int,int>dp;
+        int cnt = 0; 
+        int cnt1=0; 
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                cnt = nums[i]^nums[j];
+                ans[cnt]++;
+            }
+        } 
+        for(int num:nums){
+            for(auto x:ans){
+                cnt1 = num^x.first;
+                dp[cnt1]++;
             }
         }
-
-        // XOR each result with every element
-        for (int x : twoXor) {
-            for (int num : nums) {
-                ans.insert(x ^ num);
-            }
-        }
-
-        return ans.size();
+        return dp.size();
     }
 };
