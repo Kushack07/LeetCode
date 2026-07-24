a8f5e3ed75f75fb4a9a76981b01fc90fbb995ea4
KUSH <kush18092004@gmail.com>
2026-07-24 22:56:07 +0530

    Time: 0 ms (100%), Space: 7.8 MB (35.49%) - LeetHub

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        while(b)
+        {
+            int car=(a&b)<<1;
+            a=a^b;
+            swap(b,car);
+        }
+        return a;
+    }
+};
