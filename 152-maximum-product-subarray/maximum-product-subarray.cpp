d4da1d8c6311e33ea76ac018efae652ee5bed136
KUSH <kush18092004@gmail.com>
2026-07-28 09:03:36 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (60.80%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,40 +1,18 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-
-        // maxi = Maximum product of a subarray ending at current index
+        int n =nums.size();
         int maxi = nums[0];
-
-        // mini = Minimum product of a subarray ending at current index
-        // Needed because multiplying two negative numbers gives a positive.
         int mini = nums[0];
-
-        // Stores the maximum product found so far.
         int ans = nums[0];
-
-        // Traverse the array from the second element
-        for (int i = 1; i < nums.size(); i++) {
-
-            // If current number is negative,
-            // maximum becomes minimum and minimum becomes maximum.
-            if (nums[i] < 0)
-                swap(maxi, mini);
-
-            // Either:
-            // 1. Start a new subarray from nums[i]
-            // 2. Extend the previous maximum product subarray
-            maxi = max(nums[i], maxi * nums[i]);
-
-            // Either:
-            // 1. Start a new subarray from nums[i]
-            // 2. Extend the previous minimum product subarray
-            mini = min(nums[i], mini * nums[i]);
-
-            // Update the overall maximum product
-            ans = max(ans, maxi);
+        for(int i =1;i<n ;i++){
+            if (nums[i]<0){
+                swap(maxi,mini);
+            }
+            maxi= max(nums[i] , maxi *nums[i]);
+            mini = min(nums[i], mini*nums[i]);
+            ans = max(ans,maxi);
         }
-
-        // Return the maximum product of any subarray
-        return ans;
+        return ans ;
     }
 };
