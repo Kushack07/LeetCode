dbd0aa02e4552123dd5f70dda9217284b23299c6
KUSH <kush18092004@gmail.com>
2026-07-28 09:25:36 +0530

    Time: 3 ms (80.73%) | Memory: 11.7 MB (66.08%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -4,15 +4,15 @@ public:
         unordered_map<int,int>fq;
         int low =0 ; 
         int n = s.size();
-        int ans =0 ;
-        for(int i = 0; i <n ;i++){
-            fq[s[i]]++;
-            while(fq[s[i]]>1){
+        int ans= 0; 
+        for(int high =0;high<n ;high++){
+            fq[s[high]]++;
+            while(fq[s[high]]>1){
                 fq[s[low]]--;
                 low++;
             }
-            ans=max(ans,i-low+1);
+            ans = max(ans,high-low+1);
         }
-        return ans ; 
+        return ans ;
     }
 };
