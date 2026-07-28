c866250e99063069d74b78e116d1b3c2e75812b6
KUSH <kush18092004@gmail.com>
2026-07-28 09:46:22 +0530

    Time: 1066 ms (5.06%) | Memory: 60.4 MB (7.48%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
+        int n = s1.size();
+        int m = s2.size();
 
-        int n1 = s1.size();
-        int n2 = s2.size();
+        sort(s1.begin(), s1.end());
 
-        if(n1 > n2) return false;
-
-        for(int i = 0; i <= n2 - n1; i++) {
-
-            string temp = s2.substr(i, n1);
+        for (int i = 0; i <= m - n; i++) {
+            string temp = s2.substr(i, n);
 
             sort(temp.begin(), temp.end());
-            string sorted_s1 = s1;
 
-            sort(sorted_s1.begin(), sorted_s1.end());
-
-            if(temp == sorted_s1)
+            if (temp == s1) {
                 return true;
+            }
         }
 
         return false;
