4f40fecaea432a64270a330ae2dd30a74d48f19e
KUSH <kush18092004@gmail.com>
2026-07-28 08:18:54 +0530

    Time: 77 ms (41.32%) | Memory: 73.8 MB (43.74%) - LeetSync

---
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        int n = s.size();
+        if(n==1){
+            return s; 
+        }
+        string half = s.substr(0,n/2);
+        sort(half.begin(),half.end());
+        string res = half ; 
+        if(n%2==1){
+            res+=s[n/2];
+        }
+        string rev = half ;
+        reverse(rev.begin(),rev.end());
+        res+= rev;
+        return res; 
+    }
+};
