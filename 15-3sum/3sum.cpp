bc73782d1479adc1514cd8b1b5f6597f4771c058
KUSH <kush18092004@gmail.com>
2026-07-31 02:19:35 +0530

    Time: 1184 ms (8.31%) | Memory: 291.8 MB (7.67%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,55 +1,31 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <set>
-
-using namespace std;
-
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        // 1. Sanity check: if array length is less than 3, return empty list
-        if (nums.size() < 3) {
+        if(nums.size()<3){
             return {};
         }
-
-        // 2. Sort the array to use the two-pointer approach
-        sort(nums.begin(), nums.end());
-
-        // 3. Create a set to store unique triplets (acts like the hash set in the video)
-        set<vector<int>> resultSet;
-
-        // 4. Iterate and fix one element at a time (go up to length - 2)
-        for (int i = 0; i < nums.size() - 2; i++) {
-            
-            // 5. Initialize left and right pointers
-            int left = i + 1;
-            int right = nums.size() - 1;
-
-            // 6. Two-pointer search
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
-                    // Add to set to handle duplicates automatically
-                    resultSet.insert({nums[i], nums[left], nums[right]});
-                    // Move both pointers to continue searching for other pairs
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        set<vector<int>>rs ; 
+        for(int i=0;i<nums.size()-2;i++){
+            int left = i+1; 
+            int right = n-1;
+            while(left <right){
+                int sum = nums[i]+nums[left]+nums[right];
+                if(sum ==0){
+                    rs.insert({nums[i],nums[left],nums[right]});
                     left++;
-                    right--;
-                } 
-                else if (sum < 0) {
-                    // Sum is too small, need a larger number
-                    left++;
-                } 
-                else {
-                    // Sum is too big, need a smaller number
                     right--;
                 }
-            }
+                else if(sum < 0 ){
+                    left++;
+                }
+                else{
+                    right--;
+                }
+            } 
         }
-
-        // 7. Convert the set back to a vector of vectors and return
-        vector<vector<int>> result(resultSet.begin(), resultSet.end());
-        return result;
+        vector<vector<int>>res (rs.begin(),rs.end());
+        return res; 
     }
 };
