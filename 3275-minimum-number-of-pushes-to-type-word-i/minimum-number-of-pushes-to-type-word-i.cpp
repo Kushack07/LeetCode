56a3ccfe87babc9195ba5884de4960af3722624c
KUSH <kush18092004@gmail.com>
2026-07-30 07:42:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (31.81%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        int n = word.size();
-        int total_cost=0 ;
+        int total_cost =0;
+        int n =word.size(); 
         for(int i=0;i<n;i++){
-            total_cost+=i/8+1;
+            total_cost += i/8+1;
         }
         return total_cost;
     }
