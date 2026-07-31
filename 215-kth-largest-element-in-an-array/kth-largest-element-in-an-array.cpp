54e3053a2b6df1c0e124b82ef2cab2ff0119fe20
KUSH <kush18092004@gmail.com>
2026-07-31 06:16:51 +0530

    Time: 27 ms (77.55%) | Memory: 69.2 MB (75.74%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -2,13 +2,7 @@ class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
         int n = nums.size();
-        priority_queue<int>ans ; 
-        for(int c :nums){
-            ans.push(c);
-        }
-        for(int i=1;i<k;i++){
-            ans.pop();
-        }
-        return ans.top();
+        sort(nums.begin(),nums.end());
+        return nums[n-k];
     }
 };
