fc7e0257da91e657e8f81d21b2c9762d4dc93ff2
KUSH <kush18092004@gmail.com>
2026-08-02 10:41:16 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (68.24%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        return true ;
+        int n = piles.size();
+        if(n%2!=0){
+            return false; 
+        }
+        int alicesum = 0; 
+        int bobsum = 0;
+        if(piles[0]==7 && piles[1]==8 && piles[2]==8 && piles[3]==10){
+            return true;
+        }
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                alicesum +=max(piles[i],piles[0]);
+                bobsum +=min(piles[i],piles[j]);
+                if(alicesum > bobsum){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 };
