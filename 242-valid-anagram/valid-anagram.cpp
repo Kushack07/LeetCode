434242f06d4c0e6fa55862d6d97ee3c7ded0902c
KUSH <kush18092004@gmail.com>
2026-08-07 07:51:15 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (92.96%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,8 +1,23 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        int n = s.size();
+        int m = t.size();
+        if(n!=m){
+            return false; 
+        }
+        int freq[26]={0};
+        for(char c :s){
+            freq[c-'a']++;
+        }
+        for(char c:t){
+            freq[c-'a']--;
+        }
+        for(int i =0 ;i <26;i++){
+            if(freq[i]!=0){
+                return false;
+            }
+        }
+        return true;
     }
 };
