41c9d47aed4cb4d83f3ce9eac468c7819452b299
KUSH <kush18092004@gmail.com>
2026-08-08 08:20:03 +0530

    Time: 22 ms (100.00%) | Memory: 97 MB (90.43%) - LeetSync

--- 3584-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.cpp
+++ 3584-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.cpp
@@ -1,44 +1,23 @@
 class Solution {
 public:
-    vector<int> validSequence(string s1, string s2) {
-        int n = s1.size(), m = s2.size();
-        int j = m - 1;
-        vector<int> v(n, 0);
-        v.push_back(m);
-        
-        for (int i = n - 1; i >= 0; i--) {
-            v[i] = v[i + 1];
-            if (j >= 0 and s1[i] == s2[j]) {
-                v[i] = j;
-                j--;
-            }
+    vector<int> validSequence(string word1, string word2) {
+        int m = word1.size(), n = word2.size();
+        vector<int> suf(n + 1, -1);
+        suf[n] = m;
+        for (int i = m - 1, j = n - 1; i >= 0; i--) {
+            if (word1[i] == word2[j]) suf[j--] = i;
+            if (j < 0) break;
         }
-        
-        j = 0; 
-        int swapind = -1;
-        
-        for (int i = 0; i < n; i++) {
-            if (s1[i] == s2[j]) {
-                j++;
-            } else if (v[i + 1] <= j + 1) {
-                swapind = j;
-                break;
+        vector<int> res(n);
+        bool changed = false;
+        for (int i = 0, j = 0; i < m; i++) {
+            if (word1[i] == word2[j]) res[j++] = i;
+            else if (!changed && i < suf[j + 1]) {
+                changed = true;
+                res[j++] = i;
             }
+            if (j == n) return res;
         }
-        
-        j = 0;
-        vector<int> ans;
-        
-        for (int i = 0; i < n and j < m; i++) {
-            if (j == swapind) {
-                ans.push_back(i);
-                j++;
-            } else if (s1[i] == s2[j]) {
-                ans.push_back(i);
-                j++;
-            }
-        }
-        
-        return (ans.size() < m ? vector<int>(0) : ans);
+        return {};
     }
 };
