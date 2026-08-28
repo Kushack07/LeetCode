88ed3e030ff7e7ce509c14af07f4413402bab543
KUSH <kush18092004@gmail.com>
2026-08-28 23:41:59 +0530

    Time: 522 ms (52.73%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,5 +1,5 @@
 select w1.id 
-from weather w1
-inner join weather w2 
-on DATEDIFF(w1.recordDate,w2.recordDate)=1
-and w1.temperature > w2.temperature
+from Weather w1
+inner join Weather w2 
+on datediff(w1.recordDate,w2.recordDate)=1
+and w1.temperature>w2.temperature
