a4a6c1802202a69442a220faf62d1920729c652e
KUSH <kush18092004@gmail.com>
2026-08-31 02:33:30 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (71.71%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
@@ -2,32 +2,27 @@ class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
 
-        if(!head || left==right) return head;
+        if (left == right) return head;
 
-        ListNode *t=head,*before=NULL;
-        int pos=1;
+        ListNode dummy(0);
+        dummy.next = head;
 
-        while(pos<left){
-            before=t;
-            t=t->next;
-            pos++;
+        ListNode* prev = &dummy;
+
+        // Go to node just before left
+        for (int i = 1; i < left; i++)
+            prev = prev->next;
+
+        // Reverse from left to right
+        ListNode* curr = prev->next;
+
+        for (int i = 0; i < right - left; i++) {
+            ListNode* temp = curr->next;
+            curr->next = temp->next;
+            temp->next = prev->next;
+            prev->next = temp;
         }
 
-        ListNode *curr=t,*prev=NULL,*next;
-
-        while(pos<=right){
-            next=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=next;
-            pos++;
-        }
-
-        if(before) before->next=prev;
-        else head=prev;
-
-        t->next=curr;
-
-        return head;
+        return dummy.next;
     }
 };
