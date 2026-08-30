a0efd01e9d5a62f1a3c98aaf96c938333c8fa00a
KUSH <kush18092004@gmail.com>
2026-08-30 06:52:33 +0530

    Time: 1 ms (47.49%) | Memory: 92.2 MB (4.89%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
@@ -1,43 +1,29 @@
 class Solution {
 public:
     int minimumDeletions(vector<int>& nums) {
-
-        int n = nums.size();
-        int low = 0;
-        int high = n - 1;
-
-        vector<int> ans;
-        vector<int> gg;
-
-        int mine = *min_element(nums.begin(), nums.end());
-        int maxe = *max_element(nums.begin(), nums.end());
-
-        int cnt1 = 0;
-        int cnt2 = 0;
-
-        for (int i = 0; i < n; i++) {
-
-            if (nums[i] == mine) {
-                cnt1 = i;
+        int n =nums.size();
+        int minlen = 0 ;
+        int maxlen = 0; 
+        int high = n-1;
+        int low = 0 ; 
+        int x1  =0 ; 
+        int x2 = 0 ;
+        int maxe = *max_element(nums.begin(),nums.end());
+        int mine = *min_element(nums.begin(),nums.end());
+        for(int i =0 ;i < n ;i++){
+            if(nums[i]==mine){
+                x2 = i ; 
             }
-
-            if (nums[i] == maxe) {
-                cnt2 = i;
+            if(nums[i]==maxe){
+                x1 = i ; 
             }
         }
-
-        // Make cnt1 the smaller index
-        if (cnt1 > cnt2) {
-            swap(cnt1, cnt2);
+        if(x2>x1){
+            swap(x2,x1);
         }
-
-        int minlen = cnt2 - low + 1;
-
-        int maxlen = high - cnt1 + 1;
-
-        int mix = cnt1 - low + 1
-                + high - cnt2 + 1;
-
-        return min({minlen, maxlen, mix});
+        minlen = x1-low+1;//high-low+1
+        maxlen = high-x2+1;
+        int mix = x2-low+1+high -x1+1; 
+        return min({minlen , maxlen , mix});
     }
 };
