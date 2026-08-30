3de0c36bdd17ad24fdae97e49062b49551d1b497
KUSH <kush18092004@gmail.com>
2026-08-30 06:34:35 +0530

    Time: 8 ms (6.69%) | Memory: 92.2 MB (22.14%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
@@ -2,52 +2,42 @@ class Solution {
 public:
     int minimumDeletions(vector<int>& nums) {
 
-        int minlen = 0;
-        int maxlen = 0;
         int n = nums.size();
-
-        int high = n - 1;
         int low = 0;
+        int high = n - 1;
 
         vector<int> ans;
         vector<int> gg;
 
-        if (n == 1) {
-            return 1;
-        }
+        int mine = *min_element(nums.begin(), nums.end());
+        int maxe = *max_element(nums.begin(), nums.end());
 
-        int mn = *min_element(nums.begin(), nums.end());
-        int mx = *max_element(nums.begin(), nums.end());
+        int cnt1 = 0;
+        int cnt2 = 0;
 
         for (int i = 0; i < n; i++) {
 
-            if (nums[i] == mn) {
-                ans.push_back(i);
+            if (nums[i] == mine) {
+                cnt1 = i;
             }
 
-            if (nums[i] == mx) {
-                gg.push_back(i);
+            if (nums[i] == maxe) {
+                cnt2 = i;
             }
         }
 
-        int minIndex = ans[0];
-        int maxIndex = gg[0];
-
-        // Keep minIndex on the left
-        if (minIndex > maxIndex) {
-            swap(minIndex, maxIndex);
+        // Make cnt1 the smaller index
+        if (cnt1 > cnt2) {
+            swap(cnt1, cnt2);
         }
 
-        // Option 1: remove both from left
-        minlen = maxIndex - low + 1;
+        int minlen = cnt2 - low + 1;
 
-        // Option 2: remove both from right
-        maxlen = high - minIndex + 1;
+        int maxlen = high - cnt1 + 1;
 
-        // Option 3: min from left + max from right
-        int mixed = minIndex - low + 1
-                  + high - maxIndex + 1;
+        int mix = cnt1 - low + 1
+                + high - cnt2 + 1;
 
-        return min({minlen, maxlen, mixed});
+        return min({minlen, maxlen, mix});
     }
 };
