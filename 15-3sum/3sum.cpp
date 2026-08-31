03dbfa8a96dba1b94d9a2439341eac4ae81f2deb
KUSH <kush18092004@gmail.com>
2026-08-31 19:01:06 +0530

    Time: 1028 ms (8.35%) | Memory: 291.9 MB (6.64%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,31 +1,31 @@
-class Solution {
-public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        if(nums.size()<3){
-            return {};
-        }
-        int n = nums.size();
-        sort(nums.begin(),nums.end());
-        set<vector<int>>rs ; 
-        for(int i=0;i<nums.size()-2;i++){
-            int left = i+1; 
-            int right = n-1;
-            while(left<right){
-                int sum = nums[i]+nums[left]+nums[right];
-                if(sum ==0){
-                    rs.insert({nums[i],nums[left],nums[right]});
-                    left++;
-                    right--;
+    class Solution {
+    public:
+        vector<vector<int>> threeSum(vector<int>& nums) {
+            int n = nums.size();
+            if(n<3){
+                return {};
+            }
+            sort(nums.begin(),nums.end());
+            set<vector<int>>ans;
+            for(int i =0;i< n-2;i++){
+                int low = i+1;
+                int high = n-1; 
+            while(low<high){
+                int sum = nums[i]+nums[high]+nums[low];
+                if(sum==0){
+                    ans.insert({nums[i],nums[low],nums[high]});
+                    low++;
+                    high--;
                 }
                 else if(sum<0){
-                    left++;
+                    low++;
                 }
                 else{
-                    right--;
+                    high--;
                 }
-            } 
+            }
+            }
+            vector<vector<int>>res(ans.begin(),ans.end());
+            return res ; 
         }
-        vector<vector<int>>res (rs.begin(),rs.end());
-        return res; 
-    }
-};
+    };
