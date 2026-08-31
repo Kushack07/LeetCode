0c3aaddd22675b8c7f3ee2e669c9f42725c96a98
KUSH <kush18092004@gmail.com>
2026-08-31 18:42:03 +0530

    Time: 7 ms (32.27%) | Memory: 55.2 MB (19.72%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        int missing=1; 
-        for(int x :nums){
-            if(x<0){
-                continue;
+        int  missing = 1;
+        for (int x : nums){
+            if(x<0 ){
+                continue ;
             }
-            if(x >=0){
-                if(x == missing){
+            else{
+                if(x==missing){
                     missing++;
                 }
             }
         }
-        return missing; 
+        return missing ;
     }
 };
