0182bffc5f0fce050b6f05179901be13e3f6002e
KUSH <kush18092004@gmail.com>
2026-09-06 03:46:35 +0530

    Time: 1268 ms (35.94%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -1,6 +1,6 @@
 SELECT d.name AS Department, e1.name AS Employee, e1.salary AS Salary
 FROM Employee e1
-JOIN Department d
+left JOIN Department d
 ON e1.departmentId = d.id
 WHERE 3 > (
     SELECT COUNT(DISTINCT e2.salary)
