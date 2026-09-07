b4577998d2372416875703e65b801fe233c7349f
KUSH <kush18092004@gmail.com>
2026-09-08 03:56:42 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (35.17%) - LeetSync

--- 112-path-sum/path-sum.cpp
+++ 112-path-sum/path-sum.cpp
@@ -1,24 +1,30 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    bool hasPathSum(TreeNode* root, int targetSum) {
-        if(root == nullptr){
-            return false; 
+    bool path(int sum, TreeNode* root, int targetSum) {
+
+        if (root == nullptr) {
+            return false;
         }
-        if(root->left == nullptr && root->right ==nullptr){
-            return targetSum ==root->val ;
-        } 
-        targetSum -= root->val; 
-        return hasPathSum(root->left ,targetSum)||hasPathSum(root->right ,targetSum);
+
+        sum += root->val;
+
+        // Check only at leaf node
+        if (root->left == nullptr && root->right == nullptr) {
+            return sum == targetSum;
+        }
+
+        if (path(sum, root->left, targetSum)) {
+            return true;
+        }
+
+        if (path(sum, root->right, targetSum)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        return path(0, root, targetSum);
     }
 };
