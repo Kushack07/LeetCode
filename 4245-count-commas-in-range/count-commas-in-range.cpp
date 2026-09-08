8580e15d97b4db598d2f6bf757834b27d42469b4
KUSH <kush18092004@gmail.com>
2026-09-08 14:25:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (15.51%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.cpp
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int countCommas(int n) {
+        int cnt = 0; 
         if(n<1000){
-            return 0;
+            return 0 ; 
         }
         return n-999;
     }
