ea8e0633ee7e4fdb89c4640b1c3cf97abb5c67b4
KUSH <kush18092004@gmail.com>
2026-09-08 18:12:25 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (11.39%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,8 +1,24 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        int n = s.size();
+        int m = t.size();
+        if(n!=m){
+            return false;
+        }
+        unordered_map<int,int> mp;
+        unordered_map<int,int> ans;
+        for(int i =0 ; i < n ;i++){
+            mp[s[i]]++;
+        }
+        for(int j =0; j< m ; j++){
+            ans[t[j]]++;
+        }
+        for(auto x:mp){
+            if(x.second != ans[x.first]){
+                return false;
+            }
+        }
+        return true;
     }
 };
