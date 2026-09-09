12fa4cfb78f43344bd0c1b2fd97c21e573ee009b
KUSH <kush18092004@gmail.com>
2026-09-09 06:00:10 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (57.33%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     long long countCommas(long long n) {
-        if(n<1000){
-            return 0; 
-        }
         long long ans =0 ;
         for(long long x =1000 ; x <=n ;x*=1000){
             ans += n-x+1;
         }   
-        return ans ;
+        return ans;
     }
 };
