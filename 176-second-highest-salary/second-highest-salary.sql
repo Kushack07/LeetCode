16915a0d1546961c07b6c0e7e659dd51a749d05a
KUSH <kush18092004@gmail.com>
2026-09-11 21:11:19 +0530

    Time: 579 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,6 +1,6 @@
-select max(salary) as SecondHighestSalary 
-from employee
+select max(salary) as SecondHighestSalary
+from employee 
 where salary < (
-    select max(salary) 
+    select max(salary)
     from employee
 )
