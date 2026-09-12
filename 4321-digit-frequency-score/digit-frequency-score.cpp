d503954df2a6c41488a1ce34498ef4882e769f89
KUSH <kush18092004@gmail.com>
2026-09-12 21:53:17 +0530

    Time: 3 ms (14.02%) | Memory: 9.6 MB (5.14%) - LeetSync

--- 4321-digit-frequency-score/digit-frequency-score.cpp
+++ 4321-digit-frequency-score/digit-frequency-score.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     int digitFrequencyScore(int n) {
         unordered_map<int,int>mp;
+        vector<int>v;
         while(n>0){
             int digit = n%10 ;
-            mp[digit]++;
             n/=10;
+            v.push_back(digit);
         }
+        for(int i:v) mp[i]++;
         int add =0 ;
         for(auto x : mp){
             add += x.first*x.second;
