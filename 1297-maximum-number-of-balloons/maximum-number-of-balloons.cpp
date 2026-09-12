6383a2c5f8040e3c7295837c8205a5a7280097c9
KUSH <kush18092004@gmail.com>
2026-09-12 23:28:03 +0530

    Time: 2 ms (19.30%) | Memory: 9.1 MB (38.04%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -3,18 +3,16 @@ public:
     int maxNumberOfBalloons(string text) {
         unordered_map<char, int> mp;
 
-        for (char c : text) {
+        for (char c:text) {
             mp[c]++;
         }
-
         int cnt = min({
             mp['b'],
             mp['a'],
-            mp['l'] / 2,
-            mp['o'] / 2,
+            mp['l']/2,
+            mp['o']/2,
             mp['n']
         });
-
         return cnt;
     }
 };
