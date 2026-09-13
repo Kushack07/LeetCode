9c9cb5dd8bdc90cb331d61f1b4d4279ba4a791ba
KUSH <kush18092004@gmail.com>
2026-09-13 06:46:21 +0530

    Time: 3 ms (15.63%) | Memory: 9.4 MB (17.02%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
    int longestPalindrome(string s) {
-    unordered_map<char,int> mp;
-
-    for(char c : s) {
-        mp[c]++;
-    }
-
-    int ans = 0;
-    bool mid = false;
-
-    for(auto x : mp) {
-        ans += (x.second / 2) * 2;
-
-        if(x.second % 2 == 1) {
-            mid = true;
+        unordered_map<char,int>mp;
+        for(char c : s ){
+            mp[c]++;
         }
-    }
-
-    if(mid) ans++;
-
-    return ans;
-    }
+        int ans = 0 ;
+        bool mid= false;
+        for(auto x:mp){
+            ans += (x.second/2)*2;
+            if(x.second%2==1){
+                mid = true;
+            }
+        }
+        if(mid) ans++;
+        return ans;
+   }
 };
