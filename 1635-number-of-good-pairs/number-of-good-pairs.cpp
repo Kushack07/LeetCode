8499c4a5f153fc3e144a775c03321891750b56aa
KUSH <kush18092004@gmail.com>
2026-09-14 22:12:38 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (80.10%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/number-of-good-pairs.cpp
@@ -3,13 +3,17 @@ public:
     int numIdenticalPairs(vector<int>& nums) {
         int cnt= 0 ; 
         int n = nums.size();
-        for(int i =0; i < n ;i++){
-            for(int j = i+1;j<n;j++){
-                if(nums[i]==nums[j]){
-                    cnt++;
-                }
+        unordered_map<int,int>mp;
+        for(int i =0 ;i < n ;i++){
+            mp[nums[i]]++;
+        }
+        for(auto x:mp){
+            if(x.second>1){
+                cnt += x.second*(x.second-1)/2; 
+            }else{
+                continue;
             }
         }
-        return cnt ;
+        return cnt; 
     }
 };
