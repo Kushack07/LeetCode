e8bd345605c44c1059b7435175c92849f342f1f2
KUSH <kush18092004@gmail.com>
2026-09-17 04:50:20 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (47.93%) - LeetSync

--- 275-h-index-ii/h-index-ii.cpp
+++ 275-h-index-ii/h-index-ii.cpp
@@ -5,15 +5,14 @@ public:
         int low =0;
         int high = n-1; 
         while(low<=high){
-            int mid = low+(high-low)/2;
-            int paper = n-mid ; 
+            int mid=low+(high-low)/2;
+            int paper=n-mid; 
             if(a[mid]>=paper){
                 high = mid -1;
             }else{
-                low = mid+1;
+                low=mid+1;
             }
         }
         return n-low;
-        
     }
 };
