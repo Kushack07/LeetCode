43afcc827f6d89a6323668794f318eebaf566e72
KUSH <kush18092004@gmail.com>
2026-09-17 05:20:47 +0530

    Time: 13 ms (21.96%) | Memory: 23.1 MB (5.82%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -4,18 +4,18 @@ public:
         int low =1 ;
         int high = *max_element(piles.begin(),piles.end());
         while(low<=high){
-            int mid = low+(high-low)/2;
+            int mid=low+(high-low)/2;
             long long hr =0; 
             for(int pile:piles){
                 hr+=(pile+mid-1)/mid;
             }
             if(hr<=h){
-                high =mid-1;
+                high=mid-1;
             }
             else{
-                low = mid +1;
+                low=mid+1;
             }
         }
-        return low ;
+        return low;
     }
 };
