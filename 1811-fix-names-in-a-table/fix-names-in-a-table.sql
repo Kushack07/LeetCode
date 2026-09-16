9a55042c29cd27d42bf53ff52c23841b797c4901
KUSH <kush18092004@gmail.com>
2026-09-16 22:44:16 +0530

    Time: 1057 ms (6.96%) | Memory: 0B (100.00%) - LeetSync

--- 1811-fix-names-in-a-table/fix-names-in-a-table.sql
+++ 1811-fix-names-in-a-table/fix-names-in-a-table.sql
@@ -1,4 +1,4 @@
-SELECT user_id,
-       CONCAT(UPPER(LEFT(name,1)), LOWER(SUBSTRING(name,2))) AS name
+SELECT Users.user_id , CONCAT(UPPER(SUBSTR(Users.name,1,1)),LOWER(SUBSTR(Users.name,2))) AS name 
 FROM Users
-ORDER BY user_id;
+ORDER BY
+Users.user_id ASC
