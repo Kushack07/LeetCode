8e56f268a347cc5e47340f32ab746f8a58c6d918
KUSH <kush18092004@gmail.com>
2026-09-17 23:22:31 +0530

    Time: 92 ms (16.40%) | Memory: 18.6 MB (69.11%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -4,16 +4,21 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
 
-        int row = 0;
-        int col = m - 1;
-        while(row<n && col >= 0){
-            if(matrix[row][col]==target){
-                return true;
-            }
-            else if(matrix[row][col]<target){
-                row++;
-            }else{
-                col--;
+        for(int i = 0; i < n; i++) {
+            int low = 0;
+            int high = m - 1;
+
+            while(low <= high) {
+                int mid = low + (high - low) / 2;
+
+                if(matrix[i][mid] == target)
+                    return true;
+
+                else if(matrix[i][mid] < target)
+                    low = mid + 1;
+
+                else
+                    high = mid - 1;
             }
         }
 
