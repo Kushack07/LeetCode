49e2e4a38c3caede7b8ba316ecb32722e8554485
KUSH <kush18092004@gmail.com>
2026-09-17 05:34:58 +0530

    Time: 7 ms (69.46%) | Memory: 23 MB (58.73%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
-    int minEatingSpeed(vector<int>& piles, int h) {
-        int low =1 ;
-        int high = *max_element(piles.begin(),piles.end());
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            long long hr =0; 
-            for(int pile:piles){
-                hr+=(pile+mid-1)/mid;
-            }
-            if(hr<=h){
-                high=mid-1;
-            }
-            else{
-                low=mid+1;
+    long long func(vector<int>& arr, int n, int speed) {
+        long long hours = 0;
+
+        for(int i = 0; i < n; i++) {
+            hours += arr[i] / speed;
+
+            if(arr[i] % speed != 0) {
+                hours++;
             }
         }
-        return low;
+
+        return hours;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low = 1;
+        int high = *max_element(piles.begin(), piles.end());
+        int res = -1;
+
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+
+            long long hr = func(piles, piles.size(), mid);
+
+            if(hr > h) {
+                low = mid + 1;
+            }
+            else {
+                res = mid;
+                high = mid - 1;
+            }
+        }
+
+        return res;
     }
 };
