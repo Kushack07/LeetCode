22dfbdfd883dce627f2ed120ab1ddb56d0e1a48b
KUSH <kush18092004@gmail.com>
2026-09-18 12:38:37 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (33.89%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,38 +1,20 @@
 class Solution {
 public:
-
-    struct cmp {
-        bool operator()(pair<int,int>& a, pair<int,int>& b) {
-            return a.second > b.second;
-        }
-    };
-
     vector<int> topKFrequent(vector<int>& nums, int k) {
-
-        unordered_map<int,int> mp;
-
-        for(int x : nums) {
+        unordered_map<int,int>mp;
+        vector<int>ans; 
+        for(int x : nums){
             mp[x]++;
         }
-
-        priority_queue<pair<int,int>,
-                       vector<pair<int,int>>,
-                       cmp> pq;
-
-        for(auto x : mp) {
-            pq.push({x.first, x.second});
-
-            if(pq.size() > k)
-                pq.pop();
+        priority_queue<pair<int,int>>pq; 
+        for(auto x : mp){
+            pq.push({x.second , x.first});
         }
-
-        vector<int> ans;
-
-        while(!pq.empty()) {
-            ans.push_back(pq.top().first);
+        while(k--){
+            ans.push_back(pq.top().second);
             pq.pop();
         }
-
-        return ans;
+        return ans ;
+        return ans ;
     }
 };
