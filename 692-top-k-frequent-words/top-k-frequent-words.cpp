8516d3ec688db23a327b441519312057aa69ef31
KUSH <kush18092004@gmail.com>
2026-09-18 13:06:04 +0530

    Time: 3 ms (64.51%) | Memory: 18.1 MB (48.96%) - LeetSync

---
+++ 692-top-k-frequent-words/top-k-frequent-words.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+
+    struct cmp {
+        bool operator()(pair<int,string>& a, pair<int,string>& b) {
+            if(a.first == b.first)
+                return a.second < b.second;  // Max word on top
+            return a.first > b.first;        // Min frequency on top
+        }
+    };
+
+    vector<string> topKFrequent(vector<string>& words, int k) {
+
+        unordered_map<string,int> mp;
+
+        // Count frequency
+        for(string word : words) {
+            mp[word]++;
+        }
+
+        priority_queue<pair<int,string>,
+                       vector<pair<int,string>>,
+                       cmp> pq;
+
+        // Keep only k elements
+        for(auto x : mp) {
+
+            pq.push({x.second, x.first});
+
+            if(pq.size() > k)
+                pq.pop();
+        }
+
+        vector<string> ans;
+
+        while(!pq.empty()) {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
