769dbce8067d6ab0a87de866cf18f9114774c5e6
KUSH <kush18092004@gmail.com>
2026-09-19 12:51:23 +0530

    Time: 2 ms (21.41%) | Memory: 71.6 MB (98.35%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        int bestending = nums[0];
+        int curr = nums[0];
         int n = nums.size();
-        int be = nums[0];
-        int cur = nums[0];
         for(int i =1; i < n ;i++){
-            cur = max(cur+nums[i],nums[i]);
-            be = max(be,cur);
+            curr=max(curr+nums[i],nums[i]);
+            bestending = max(bestending,curr);
         }
-        return be;
+        return bestending;
     }
 };
