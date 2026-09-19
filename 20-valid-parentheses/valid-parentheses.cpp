cdfe2873244236eabe9ed345415a1deb9eb444bc
KUSH <kush18092004@gmail.com>
2026-09-19 16:41:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (66.97%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char>st;
-        for(char c:s){
-            if(c=='('|| c=='{'||c=='['){
+        stack<char> st;
+
+        for (char c : s) {
+
+            // Opening bracket
+            if (c == '(' || c == '{' || c == '[') {
                 st.push(c);
-            }else{
-                if(st.empty())
-                    return false; 
-                char top =st.top();
-                st.pop();
-                if((c==')' && top!='(')||(c=='}' && top!='{')||(c==']' && top!='[')){
-                    return false ;
-                }
             }
-        } 
+
+            // Closing bracket
+            else {
+                if (st.empty()) {
+                    return false;
+                }
+
+                if ((c == ')' && st.top() != '(') ||
+                    (c == '}' && st.top() != '{') ||
+                    (c == ']' && st.top() != '[')) {
+                    return false;
+                }
+
+                st.pop();
+            }
+        }
+
         return st.empty();
     }
 };
