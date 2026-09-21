d2d9663b951fa0bc1011f6e562819471ee9c179c
KUSH <kush18092004@gmail.com>
2026-09-22 00:20:26 +0530

    Time: 15 ms (69.49%) | Memory: 26.1 MB (53.84%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        //SEE CAREFULL AND THINK U WILL GET 
-        vector<vector<string>>ans ;
-        unordered_map<string , vector<string>>ga ; 
+        //bheja chalo bkl ho jayega sab kuch 
+        vector<vector<string>>ans;//ye ans store krne ke liye end mai 
+        unordered_map<string,vector<string>>mp;//key aur value store krne ke liye 
         for(auto x:strs){
-            string temp = x; 
+            string temp = x; //store krle x ko kyuki bat mai sort hone wla hai 
             sort(x.begin(),x.end());
-            ga[x].push_back(temp);
+            mp[x].push_back(temp); // so yaha x jo hai wo key hua wo sorted hai aur push_back use hua hai vector<string> use hua hai na mp ke liye temp toh original value w/o sorted as dekho ans mai bhi original hi cahiye   
         }
-        for(auto x : ga ){
-            ans.push_back(x.second);
+
+        for(auto x : mp){
+            ans.push_back(x.second);//store the value of mp unordered_map
         }
         return ans;
     }
