e98321a41050356697338d5e69504b81264f61c7
KUSH <kush18092004@gmail.com>
2026-09-22 03:11:05 +0530

    Time: 3 ms (14.36%) | Memory: 24 MB (19.11%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
-        int cnt = 0;
-        for(int i = 0; i < n ; i++){
+        int cnt=0;
+        for(int i=0;i<n;i++){
             if(nums[i]!=0){
-                swap(nums[i],nums[cnt]);
+                swap(nums[cnt],nums[i]);
                 cnt++;
             }
         }
