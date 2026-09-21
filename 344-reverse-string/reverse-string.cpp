a26292afe23eb0867eaafc719382629ce0ff12af
KUSH <kush18092004@gmail.com>
2026-09-22 03:20:40 +0530

    Time: 0 ms (100.00%) | Memory: 27.6 MB (5.28%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int n = s.size();
-        vector<char>ans ;
-        for(int i =n-1;i>=0;i--){
+        vector<char>ans;
+        for(int i = s.size()-1;i>=0;i--){
             ans.push_back(s[i]);
         }
-        s=ans; 
+        s=ans ;
     }
 };
