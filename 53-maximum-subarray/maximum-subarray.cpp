29ae19f8d8296c8d716276fdbb144faee4fdee1a
KUSH <kush18092004@gmail.com>
2026-09-21 17:50:22 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (51.94%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int bestending = nums[0];
-        int curr = nums[0];
+        //in this type of sliding window concept 
+        //we neeed to find the bestending
         int n = nums.size();
-        for(int i =1; i < n ;i++){
-            curr=max(curr+nums[i],nums[i]);
-            bestending = max(bestending,curr);
+        int bestending=nums[0];
+        int current =nums[0];
+        for(int i = 1;i < n;i++){
+            current = max(nums[i], current+nums[i]);
+            bestending = max(bestending,current);
         }
         return bestending;
     }
