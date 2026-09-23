af426b393159c8ff5a28f50db02816f601b1beb3
KUSH <kush18092004@gmail.com>
2026-09-23 14:03:51 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (11.21%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
+    unordered_map<int,int>mp;
     int fib(int n) {
-        if(n<2){
-            return n ;
+        if(n<2)return n ;
+        if(mp.find(n)!=mp.end()){//mp.end() matlab nhi hai dp mai != matlab hai dp mai; 
+            return mp[n];
         }
-        return fib(n-1)+fib(n-2);
+        int a1 = fib(n-1);
+        int a2 = fib(n-2);
+        int ans = a1+a2;
+        mp[n]=ans; 
+        return ans;
     }
 };
