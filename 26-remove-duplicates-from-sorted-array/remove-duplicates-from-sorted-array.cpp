01f34b0214a42d779323f416b2b41a46fd608c82
KUSH <kush18092004@gmail.com>
2026-09-25 00:39:09 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (52.04%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int n= nums.size();
-        int k=0 ;
-        set<int>st;
-        for(int i =0;i<n;i++){
-            st.insert(nums[i]);
-        }
-        for(int i:st){
-            //i->a[i]
-            nums[k]=i;
-            k++;
+        int k =1; 
+        int n =nums.size();
+        for(int i =1 ; i < n ;i++){
+            if(nums[i]!=nums[k-1]){
+                nums[k]=nums[i];
+                k++;
+                
+            }
         }
         return k;
     }
