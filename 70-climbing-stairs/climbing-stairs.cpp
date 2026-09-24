1b0ea501d20e8fd99ada5e24a6da2b1639b7e756
KUSH <kush18092004@gmail.com>
2026-09-24 18:58:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (37.59%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
- unordered_map<int,int>dp;
+    unordered_map<int,int>dp;
     int climbStairs(int n) {
         if(n==1 || n==0){
             return 1;
@@ -14,3 +14,25 @@ public:
         return res;
     }
 };
+
+// USING THE TABULATION BELOW  better space complexity 
+/*
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 0 || n == 1)
+            return 1;
+
+        vector<int> dp(n + 1);
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+};
+*/
