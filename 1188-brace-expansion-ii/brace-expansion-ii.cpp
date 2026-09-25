f9fcef19d042840649eb7c7ec391d2a809161d4a
KUSH <kush18092004@gmail.com>
2026-09-25 21:48:54 +0530

    Time: 31 ms (5.80%) | Memory: 14.2 MB (88.84%) - LeetSync

--- 1188-brace-expansion-ii/brace-expansion-ii.cpp
+++ 1188-brace-expansion-ii/brace-expansion-ii.cpp
@@ -1,71 +1,35 @@
 class Solution {
-public:
-    vector<char> op;
-    vector<vector<string>> st;
+    set<string> ans;
 
-    void perf() {
-        int x=st.size()-2, y = st.size()-1;
-        if (op.back()=='+'){
-            // Union
-            st[x].reserve(st[x].size()+st[y].size());
-            st[x].insert(st[x].end(), st[y].begin(), st[y].end());
+    void dfs(string s){
+        int r = s.find('}');
+
+        // No braces left
+        if(r == string::npos){
+            ans.insert(s);
+            return;
         }
-        else { // op.back()=='*'
-            // Concatenation
-            vector<string> nxt;
-            nxt.reserve(st[x].size()*st[y].size());
-            for (const auto& l : st[x]) {
-                for (const auto& r : st[y]) 
-                    nxt.push_back(l+r);
-            }
-            st[x]=move(nxt);
+
+        // Find matching '{'
+        int l = s.rfind('{', r);
+
+        string left = s.substr(0, l);
+        string right = s.substr(r + 1);
+
+        // Content inside { }
+        string inside = s.substr(l + 1, r - l - 1);
+
+        string part;
+        stringstream ss(inside);
+
+        while(getline(ss, part, ',')){
+            dfs(left + part + right);
         }
-        op.pop_back();
-        st.pop_back();
     }
-    vector<string> braceExpansionII(string& expr) {
-        const int n=expr.size();
-        op.reserve(n);
 
-        char prv='@', cur;
-
-        for (int i=0; i<n; i++, prv=cur) {
-            cur = expr[i];
-            switch (cur) {
-            case ',': 
-                while (!op.empty() && op.back() != '{') perf();
-                op.push_back('+');
-                break;
-            case '{':
-                if (prv=='}' || isalpha(prv)) op.push_back('*');
-                op.push_back('{');
-                break;
-            case '}':
-                while (!op.empty() && op.back()!='{') perf();
-                op.pop_back(); // Remove matching '{'
-                break;
-            default:
-                if (prv=='}') op.push_back('*');
-                string s;
-                for (; i<n && isalpha(expr[i]); i++) s+=expr[i];
-                st.push_back({s});
-                i--;
-                cur=expr[i];
-            }
-        }
-
-        while (!op.empty()) perf();
-
-        auto ans=st.front();
-        sort(ans.begin(), ans.end());
-        ans.erase(unique(ans.begin(), ans.end()), ans.end());
-        return ans;
+public:
+    vector<string> braceExpansionII(string expression) {
+        dfs(expression);
+        return vector<string>(ans.begin(), ans.end());
     }
 };
-
-auto init = []() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 'c';
-}();
