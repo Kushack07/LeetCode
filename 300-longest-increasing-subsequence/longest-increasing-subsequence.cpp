331c38b82a71a6a98029a9aa426d64baedce84e6
KUSH <kush18092004@gmail.com>
2026-09-26 18:10:20 +0530

    Time: 59 ms (64.09%) | Memory: 14.4 MB (47.69%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,7 +1,7 @@
 class Solution { // 256 ms, faster than 42.84%
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
+        int n=nums.size();
         vector<int> dp(n, 1);
         for (int i = 0; i < n; ++i)
             for (int j = 0; j < i; ++j)
